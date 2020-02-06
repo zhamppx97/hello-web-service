2b4232456b2be2c43735e11f390b0a5e6fab6b95
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-02-06 20:05:57 +0700

    ADD : Desktop client application

---
+++ desktop-client/Index.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace desktop_client
+{
+    partial class Index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelService = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelService
+            // 
+            this.labelService.AutoSize = true;
+            this.labelService.Location = new System.Drawing.Point(12, 9);
+            this.labelService.Name = "labelService";
+            this.labelService.Size = new System.Drawing.Size(35, 13);
+            this.labelService.TabIndex = 0;
+            this.labelService.Text = "label1";
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 103);
+            this.Controls.Add(this.labelService);
+            this.Name = "Index";
+            this.Text = "Index";
+            this.Load += new System.EventHandler(this.Index_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelService;
+    }
+}
+
---
+++ desktop-client/Index.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktop_client
+{
+    public partial class Index : Form
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void Index_Load(object sender, EventArgs e)
+        {
+            MyServiceReference.MyServiceSoapClient service = new MyServiceReference.MyServiceSoapClient();
+            labelService.Text = service.HelloService();
+        }
+    }
+}
